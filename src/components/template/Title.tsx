d9e78fe8fa867215c4de312245c36b51a054753d
Daniel Lucas <doliveira-g4f@abdi.com.br>
2024-06-19 18:13:54 -0300

    style adjust

--- src/components/template/Title.tsx
+++ src/components/template/Title.tsx
@@ -7,7 +7,7 @@ export default function Title(params : TitleProps) {
     return (
         <div>
             <h1 className="font-black text-3xl text-gray-900 dark:text-gray-200">{params.titulo}</h1>
-            <h2 className="font-light text-sm text-gray-600 dark:text-gray-400">{params.subtitulo}</h2>
+            <h2 className="font-light text-sm text-gray-600 dark:text-gray-400 mt-1">{params.subtitulo}</h2>
         </div>
     )
 };
