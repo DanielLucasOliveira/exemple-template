5889c8e84408dfed7e7a80c7c166fa7ebb606f7d
Daniel Lucas <doliveira-g4f@abdi.com.br>
2024-06-24 14:38:21 -0300

    add tabs to home

---
+++ src/components/template/home/Finans.tsx
@@ -0,0 +1,11 @@
+interface FinansProps {
+
+}
+
+export default function Finans(props: FinansProps) {
+    return (
+        <div>
+            <h1>Home Finans</h1>
+        </div>
+    )
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/template/Layout";
+import Finans from "@/components/template/home/Finans";
+import useMenuData from "@/data/hook/useMenuData";
 
 export default function Home() {
+  const { selectedButton } = useMenuData();
+
+    const componentMap: { [key: string]: JSX.Element } = {
+      'Finans': <Finans/>,
+      'Invest': <div><h1>Home Invest</h1></div>,
+      'Report': <div><h1>Home Report</h1></div>,
+    };
+
   return (
 
     <Layout>
-      <h1>Home Page</h1>
+      {componentMap[selectedButton]}
     </Layout>
-
   );
 }
+
