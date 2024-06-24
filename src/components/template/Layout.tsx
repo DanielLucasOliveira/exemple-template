64f178b47a5ac9a30149e0c4717a48d156120908
Daniel Lucas <doliveira-g4f@abdi.com.br>
2024-06-24 14:36:23 -0300

    adjust imports

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,29 +1,28 @@
 import useAppData from "@/data/hook/useAppData"
 import Content from "./Content"
 import Header from "./header/Header"
-import SideMenu from "./SideMenu"
 import ForceAuthentication from "../auth/ForceAuthentication"
-import { useState } from "react"
-import { Button } from "../ui/button"
-import { CloseMenuIcon, OpenMenuIcon } from "@/icons"
+import { ReactNode } from "react"
+import SideMenu from "./header/SideMenu"
+
 
 interface LayoutProps {
-    children?: any
+    children: ReactNode;
 }
 
-export default function Layout(params: LayoutProps) {
-
-    const { theme, menuState } = useAppData()
+export default function Layout({ children }: LayoutProps) {
+    const { theme, menuState } = useAppData();
 
     return (
         <ForceAuthentication activate={false}>
             <div className={`${theme} flex h-screen w-screen`}>
                 {menuState && <SideMenu />}
                 <div className="flex flex-col w-full p-4 bg-gray-200 dark:bg-gray-800">
-                    <Header/>
-                    <Content>{params.children}</Content>
+                    <Header />
+                    <Content>{children}</Content>
                 </div>
             </div>
         </ForceAuthentication>
-    )
-};
+    );
+}
+
